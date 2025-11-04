d1870770c5598a9f6f7c95a7f78565ae732b4d84
Middledot <78228142+Middledot@users.noreply.github.com>
2025-11-04 00:43:28 -0500

    feat(innit): commit

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/core";
+// import "./App.css";
+
+// function App() {
+//   const [greetMsg, setGreetMsg] = useState("");
+//   const [name, setName] = useState("");
+
+//   async function greet() {
+//     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//     setGreetMsg(await invoke("greet", { name }));
+//   }
+
+//   return (
+//     <main className="container">
+//       <h1>Welcome to Tauri + React</h1>
+
+//       <div className="row">
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+//         </a>
+//         <a href="https://tauri.app" target="_blank">
+//           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+//       <form
+//         className="row"
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           greet();
+//         }}
+//       >
+//         <input
+//           id="greet-input"
+//           onChange={(e) => setName(e.currentTarget.value)}
+//           placeholder="Enter a name..."
+//         />
+//         <button type="submit">Greet</button>
+//       </form>
+//       <p>{greetMsg}</p>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke, convertFileSrc } from "@tauri-apps/api/core";
+import { appDataDir } from "@tauri-apps/api/path";
+import "./App.css";
+
+function App() {
+  // let fileUrl = convertFileSrc(appDataDir()+"/excursions.jpg")
+
+  const [imgSrc, setImgSrc] = useState("https://f4.bcbits.com/img/a0401863863_16.jpg")
+
+
+  return (
+    <main className="higher-power">
+      <div className="general-header">
+        <h1>StoneRank</h1>
+
+        <button className="header-button" type="submit">Home</button>
+        <button className="header-button" type="submit">Downloads</button>
+        <button className="header-button" type="submit">Format</button>
+      </div>
+
+      <div className="awe-tab">
+        <div className="home">
+          <div className="cover-display">
+            <img
+              className="cover-display-image"
+              src={imgSrc}
+            />
+          </div>
+          <div className="integrated-player">
+            text
+          </div>
+          <div className="source-selector">
+            text
+          </div>
+          <div className="unranked-list"></div>
+          <div className="ranked-list"></div>
+          <div className="text-modifier"></div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
